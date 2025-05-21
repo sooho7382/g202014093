7c6dc0f62ecac52efed188bf5af23f3b78e27228
sooho7382 <sooho7382@gmail.com>
2025-05-21 09:15:04 +0900

    five

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("정수호")
         World.run();
+        World.run2();
     }
 }
--- World.java
+++ World.java
@@ -2,5 +2,8 @@ public class World {
     
     public static void run() {
         System.out.println("202014093");
+
+    public static void run2() {
+        System.out.println("hello world");
     }
 }
